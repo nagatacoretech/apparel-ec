81e47a295e1ae33048a0dfb2a56134b1f4f6f884
yu-coretech <x.yu@core-tech.jp>
2024-06-24 17:21:19 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::middleware('auth:users')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 Route::prefix('admin')->middleware('auth:admin')->group(function () {
