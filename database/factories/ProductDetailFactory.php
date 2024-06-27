ecf504da2b4d57c47718d1067e4573b61c51b848
r-eguchi0112 <r.eguchi@core-tech.jp>
2024-06-27 17:12:19 +0900

    シーダーの変更

--- database/factories/ProductDetailFactory.php
+++ database/factories/ProductDetailFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as Faker;
 use App\Models\ProductDetail;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductDetail>
@@ -18,13 +19,13 @@ class ProductDetailFactory extends Factory
      */
     protected $model = ProductDetail::class;
 
-    public function definition(): array
+    public function definition():array
     {
         return [
-            'product_id' => fake()->numberBetween(1,10),
-            'size_id' => fake()->numberBetween(1,10),
-            'color_id' => fake()->numberBetween(1,30),
-            'stock' => fake()->numberBetween(1, 10),
+            'product_id' => fake()->numberBetween(1,50),
+            'size_id' => fake()->numberBetween(1,8),
+            'color_id' => fake()->numberBetween(1,10),
+            'stock' => fake()->numberBetween(1, 50),
         ];
     }
 }
