7a553f0fda5e3abc12f1bd72e89bad9aa4ca2c0d
r-eguchi0112 <r.eguchi@core-tech.jp>
2024-06-27 10:03:33 +0900

    カート登録機能の修正

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,19 +7,35 @@ use App\Models\Cart;
 use App\Models\Product;
 use App\Models\ProductDetail;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
     public function add_cart(Request $request)
     {
+        $isValidCombination = ProductDetail::where('product_id', $request->product_id)
+        ->where('size_id', $request->size)
+        ->where('color_id', $request->color)
+        ->exists();
+
+        if (!$isValidCombination) {
+            // 在庫がない場合の処理
+            return redirect()->back()->with('error', '選択したサイズとカラーの組み合わせは在庫がありません。');
+        }
+
+        $productDetailId = ProductDetail::where('product_id', $request->product_id)
+            ->where('size_id', $request->size)
+            ->where('color_id', $request->color)
+            ->pluck('id')
+            ->first();
         // dd($request);
-        $exist_product = Cart::where('user_id',Auth::id())->where('product_detail_id',$request->product_id)->exists();
+        $exist_product = Cart::where('user_id',Auth::id())->where('product_detail_id',$productDetailId)->exists();
         if($exist_product){
-            $cart = Cart::where('product_detail_id',$request->product_id)->increment('amount',$request->amount);
+            $cart = Cart::where('product_detail_id',$productDetailId)->increment('amount',$request->amount);
         }else{
         $cart = new Cart;
         $cart->user_id = Auth::id();
-        $cart->product_detail_id = $request->product_id;
+        $cart->product_detail_id = $productDetailId;
         $cart->amount = $request->amount;
         $cart->save();
         }
@@ -29,11 +45,25 @@ class CartController extends Controller
 
     public function add_index()
     {
-        $carts = Cart::where('user_id',Auth::id())->with('product_detail.product')->get();
+        // $carts = Cart::where('user_id',Auth::id())->with('product_detail.product')->get();
+        $carts = DB::table('carts')
+            ->select('products.id','carts.product_detail_id','products.img_path', 'products.name', 'sizes.size', 'color.color', 'products.price', 'carts.amount')
+            ->join('product_details', 'carts.product_detail_id', '=', 'product_details.id')
+            ->join('products', 'product_details.product_id', '=', 'products.id')
+            ->join('sizes', 'product_details.size_id', '=', 'sizes.id')
+            ->join('color', 'product_details.color_id', '=', 'color.id')
+            ->where('carts.user_id', Auth::id())
+            ->get();
         $total_price = 0;
         foreach($carts as $cart)
         {
+<<<<<<< HEAD
+            // dd($cart);
+            // dd($cart->product_detail->product->price);
+            $total_price += $cart->price*$cart->amount;
+=======
             $total_price += $cart->product_detail->product->price*$cart->amount;
+>>>>>>> parent of 0935e24 (一度)
         }
 
         return view('carts.index',compact('carts','total_price'));
--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            @forelse($carts as $cart_products)
+            {{-- @forelse($carts as $cart_products)
                 {{$cart_products->product_detail->product->img_path}}<br>
                 {{$cart_products->product_detail->product->name}}<br>
                 ￥{{$cart_products->product_detail->product->price}}<br>
@@ -26,6 +26,38 @@
                     @endif
                 @empty
                 <p>商品はカート内にありません</p>
+            @endforelse --}}
+
+            @forelse($carts as $cart_products)
+                {{-- @dd($cart_products) --}}
+                {{$cart_products->img_path}}<br>
+                商品名：{{$cart_products->name}}<br>
+                サイズ：{{$cart_products->size}}<br>
+                カラー：{{$cart_products->color}}<br>
+                ￥{{$cart_products->price}}<br>
+                個計：{{$cart_products->price*$cart_products->amount}}<br>
+                @if(!empty($cart_products->id))
+                    <form method="POST" action="{{route('increase',$cart_products->product_detail_id)}}">
+                            @csrf
+                            <button type="submit">
+                                ➕
+                            </button>
+                    </form>
+                    {{$cart_products->amount}}<br>
+                    @if($cart_products->amount != 1)
+                        <form method="POST" action="{{route('decrease',$cart_products->product_detail_id)}}">
+                            @csrf
+                            <button id="decrementButton" class="">➖</button>
+                        </form>
+                    @endif
+                    {{-- @dd($cart_products->product_detail_id) --}}
+                <form action="{{ route('cart.remove', $cart_products->product_detail_id) }}" method="POST">
+                    @csrf
+                    <button type="submit">削除</button>
+                </form>
+                    @endif
+                @empty
+                <p>商品はカート内にありません</p>
             @endforelse
 
             <div class="my-9">
@@ -34,12 +66,20 @@
             <div class="my-9">
 
             <form action="{{route('purchase')}}" method="POST">
+            {{-- @csrf
+            @forelse($carts as $index => $cart_products)
+                <input type="hidden" name="total_price" value="{{$total_price}}">
+                <input type="hidden" name="order_items[{{$index}}][product_id]" value="{{$cart_products->product_detail->id}}">
+                <input type="hidden" name="order_items[{{$index}}][price]" value="{{$cart_products->product_detail->product->price}}">
+                <input type="hidden" name="order_items[{{$index}}][amount]" value="{{$cart_products->amount}}">
+            @empty
+            @endforelse --}}
             @csrf
             @forelse($carts as $index => $cart_products)
-            <input type="hidden" name="total_price" value="{{$total_price}}">
-            <input type="hidden" name="order_items[{{$index}}][product_id]" value="{{$cart_products->product_detail->id}}">
-            <input type="hidden" name="order_items[{{$index}}][price]" value="{{$cart_products->product_detail->product->price}}">
-            <input type="hidden" name="order_items[{{$index}}][amount]" value="{{$cart_products->amount}}">
+                <input type="hidden" name="total_price" value="{{$total_price}}">
+                <input type="hidden" name="order_items[{{$index}}][product_id]" value="{{$cart_products->product_detail_id}}">
+                <input type="hidden" name="order_items[{{$index}}][price]" value="{{$cart_products->price}}">
+                <input type="hidden" name="order_items[{{$index}}][amount]" value="{{$cart_products->amount}}">
             @empty
             @endforelse
 
